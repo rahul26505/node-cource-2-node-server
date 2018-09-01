b37778a07f84ce872ecfce328900b4cfe95cc143
extramiles <amit777bhandari@gmail.com>
2018-09-01 17:06:24 +0530

    test1

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('Server.log', log + '\n');
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.send('Hellow Express !');
